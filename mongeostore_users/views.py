13589d51eb8f3bac425d7d972299c98354719db2
genghenggo <genghenggo@outlook.com>
2020-09-23 08:00:39 +0800

    henggao

--- mongeostore_users/views.py
+++ mongeostore_users/views.py
@@ -1,3 +1,18 @@
+'''
+Description: henggao_learning
+version: v1.0.0
+Author: henggao
+Date: 2020-09-19 06:54:56
+LastEditors: henggao
+LastEditTime: 2020-09-22 15:35:22
+'''
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class UserInfoView(APIView):
+
+    def get(self,request,*args,**kwargs):
+        return Response("...")
